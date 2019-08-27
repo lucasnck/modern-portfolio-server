7c1cc55c84829de53621cb2bb87a2ae841bc43d7
Lucas Ribeiro <luucasnck@gmail.com>
2019-08-27 08:04:08 -0300

    Update info

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as env from 'dotenv'
 env.config()
 
 import app from "./modules/server";
-const PORT = 80;
+const PORT = 4600;
 app.listen(PORT, () => {
     console.log('Express server: http://localhost:' + PORT);
 }) 
